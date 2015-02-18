c8fb0c88910f625c74d74c2598f60745a6bb6070
ezaroukian <egz@jhu.edu>
2015-02-18 14:49:04 -0500

    Fill out functions for matrix caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Together, these functions allow matrix inverses to be cached and retrieved. 
+## A matrix generated with makeCacheMatrix whose inverse is calculated with cacheSolve
+## will cache its inverse, which can later be retreived through cacheSolve.
 
-## Write a short comment describing this function
 
+#Generates a inverse-cacheable matrix in the form of a list of matrix get/set and inverse get/set functions
 makeCacheMatrix <- function(x = matrix()) {
-
+  #inverse is initially NULL
+  i <- NULL
+  #set() makes a global version of the matrix and a global NULL inverse
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  #get() returns the global matrix created by set
+  get <- function() x
+  #setinver() takes the inverse and stores it globally
+  setinver <- function(inv) i <<- inv
+  #getinver() retrieves the global inverse
+  getinver <- function() i
+  #returns list of functions to get/set actual matrix and its inverse
+  list(set = set, get = get,
+       setinver = setinver,
+       getinver = getinver)
 }
 
 
-## Write a short comment describing this function
-
+# Takes a SQUARE matrix created by makeCacheMatrix (i.e. a list of get/set functions), 
+# returns its globally-cached inverse (as a list of get/set functions), if necessary first computing and caching the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #gets the globally-cached inverse
+  i <- x$getinver()
+  #if inverse has previously been computed, returns cached inverse matrix
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  #otherwise gets acutal matrix, computes inverse, stores inverse globally
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinver(i)
+  return(i)
 }
